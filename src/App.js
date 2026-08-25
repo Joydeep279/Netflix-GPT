d2539e4338807aa8ffaa8a100d30af87224a0941
Joydeep Nath <joydeepnath279@gmail.com>
2026-08-25 20:54:45 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import Body from "./Components/Body";
-import appStore from "./utils/appStore";
-
-function App() {
+const App = () => {
   return (
-    <Provider store={appStore}>
-      <Body />
-    </Provider>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
-}
+};
 
 export default App;
--- src/Components/Error.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Error = () => {
-  return <div>Oops Something Went Wrong</div>;
-};
-
-export default Error;
